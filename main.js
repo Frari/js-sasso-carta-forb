d25673222b4de116b6a81c15908c1d6816a78681
Frari <francescoriccardi86@gmail.com>
2019-04-30 15:32:22 +0200

    inizio funzione

--- main.js
+++ main.js
@@ -6,26 +6,29 @@ console.log(utente);
 var computer=Math.floor(Math.random()*3)+1;
 console.log(computer);
 
-// varie combinazioni lanci con rispettivi risultati
-if(computer==1&&utente==2){
-  console.log('computer lancia sasso');
-  console.log('hai vinto');
-}else if(computer==2&&utente==1){
-  console.log('computer lancia carta');
-  console.log('Hai perso');
-}else if(computer==3&&utente==1){
-  console.log('computer lancia forbici');
-  console.log('hai vinto');
-}else if(computer==2&&utente==3){
-  console.log('computer lancia carta');
-  console.log('hai vinto');
-}else if(computer==1&&utente==3){
-  console.log('computer lancia sasso');
-  console.log('hai vinto');
-}else if(computer==3&&utente==2){
-  console.log('computer lancia forbici');
-  console.log('hai perso');
-}else{
-  console.log('avete lanciato lo stesso segno');
-  console.log('siete pari');
+function risultato(computer, utente){
+  if(computer==1&&utente==2){
+    console.log('computer lancia sasso');
+    console.log('hai vinto');
+  }else if(computer==2&&utente==1){
+    console.log('computer lancia carta');
+    console.log('Hai perso');
+  }else if(computer==3&&utente==1){
+    console.log('computer lancia forbici');
+    console.log('hai vinto');
+  }else if(computer==2&&utente==3){
+    console.log('computer lancia carta');
+    console.log('hai vinto');
+  }else if(computer==1&&utente==3){
+    console.log('computer lancia sasso');
+    console.log('hai vinto');
+  }else if(computer==3&&utente==2){
+    console.log('computer lancia forbici');
+    console.log('hai perso');
+  }else{
+    console.log('avete lanciato lo stesso segno');
+    console.log('siete pari');
+  }
 }
+
+// varie combinazioni lanci con rispettivi risultati
